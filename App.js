ff0f8bf3391faa1787208865e3065292eb6cdf1e
Erik Ahumada <costerik@hotmail.com>
2018-09-23 22:43:22 -0500

    [SRN-002] splash configured on Android Studio

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000000',
   },
   welcome: {
     fontSize: 20,
