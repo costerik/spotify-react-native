460c6b5992b1996ed369d3c48e9a1993a1881fa9
Erik Ahumada <costerik@hotmail.com>
2018-10-04 17:25:23 -0500

    [SRN-003] added swipter component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, Image,View, Text, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import Swiper from 'react-native-swiper';
+
+//Assets
+import spotifyLogo from 'SpotifyReactNative/assets/images/Spotify_Logo_RGB_White.png';
 
 class Main extends Component {
   static propTypes = {
@@ -23,11 +27,20 @@ class Main extends Component {
         <StatusBar
           barStyle="light-content"
         />
-        <Text style={styles.text} onPress={
-          () => this.props.navigation.navigate('SignUp')
-        }>
-          Main
-        </Text>
+        {/*<Swiper autoplay activeDotColor= '#FFF'>
+          <View style={styles.slide}>
+            <Text style={styles.textSlide}>Play your favorite songs and artist.</Text>
+          </View>
+          <View style={styles.slide}>
+            <Text style={styles.textSlide}>No credit card required.</Text>
+          </View>
+          <View style={styles.slide}>
+            <Text style={styles.textSlide}>Listen on all your devices.</Text>
+          </View>
+        </Swiper>*/}
+        <View>
+          <Image source={spotifyLogo} style={styles.logo}/> 
+        </View>
       </View>
     );
   }
@@ -38,10 +51,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFF',
+    backgroundColor: '#a4ddf9',
   },
   text:{
     color: 'black',
+  },
+  textSlide: {
+
+  },
+  textSlide: {
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  logo: {
+    height: 50,
+    width: 170,
   }
 });
 
