916cf84d9af6897beccad7777713d4cb31022083
Erik Ahumada <costerik@hotmail.com>
2018-10-23 14:35:24 -0500

    [SRN-003] sign up view 60%

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image,View, Text, StatusBar } from 'react-native';
+import { StyleSheet, Image,View, Text, StatusBar, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import Swiper from 'react-native-swiper';
 
@@ -16,30 +16,41 @@ class Main extends Component {
   }
 
   static navigationOptions = {
-    //title: "Hello",
     header: null,
-    //headerTintColor: 'black',
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          barStyle="light-content"
-        />
-        {/*<Swiper autoplay activeDotColor= '#FFF'>
-          <View style={styles.slide}>
-            <Text style={styles.textSlide}>Play your favorite songs and artist.</Text>
+      <View style={styles.wrapperContainer}>
+        <View style={styles.container}>
+          <StatusBar
+            barStyle="light-content"
+          />
+          <View>
+            <Image source={spotifyLogo} style={styles.logo}/> 
           </View>
-          <View style={styles.slide}>
-            <Text style={styles.textSlide}>No credit card required.</Text>
-          </View>
-          <View style={styles.slide}>
-            <Text style={styles.textSlide}>Listen on all your devices.</Text>
-          </View>
-        </Swiper>*/}
-        <View>
-          <Image source={spotifyLogo} style={styles.logo}/> 
+          <Swiper autoplay activeDotColor= '#FFF'
+            style={styles.swiper}
+          >
+            <View style={styles.slide}>
+              <Text style={styles.textSlide}>Play your favorite songs and artist.</Text>
+            </View>
+            <View style={styles.slide}>
+              <Text style={styles.textSlide}>No credit card required.</Text>
+            </View>
+            <View style={styles.slide}>
+              <Text style={styles.textSlide}>Listen on all your devices.</Text>
+            </View>
+          </Swiper>
+          <TouchableOpacity style={[styles.button, styles.signUpButton]}>
+            <Text style={styles.buttonText}>sign up free</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.facebookButton]}>
+            <Text style={styles.buttonText}>continue with facebook</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.logInButton]}>
+            <Text style={[styles.buttonText, { color: 'black'}]}>log in</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -48,13 +59,13 @@ class Main extends Component {
 
 const styles = StyleSheet.create({
   container:{
+    alignItems: 'center',
+    margin: 10,
+  },
+  wrapperContainer:{
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#a4ddf9',
-  },
-  text:{
-    color: 'black',
   },
   textSlide: {
 
@@ -66,6 +77,32 @@ const styles = StyleSheet.create({
   logo: {
     height: 50,
     width: 170,
+  },
+  swiper: {
+  },
+  containerButtons: {
+  },
+  button: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    alignSelf: 'stretch',
+    margin: 10,
+  },
+  buttonText: {
+    textTransform: 'uppercase',
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  signUpButton:{
+    backgroundColor: 'green',
+  },
+  facebookButton:{
+    backgroundColor: '#4267b2',
+  },
+  logInButton:{
+    backgroundColor: 'white',
   }
 });
 
